9e45d54cddee2c62d0e02a10e7a21e9fe788f0f6
Francisco Eudes <eudesmendesfilho@gmail.com>
2024-07-18 19:32:54 -0300

    Added terrain and collisions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,13 +18,28 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+		if is_on_floor():
+			animated_sprite_2d.play("walk")
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		if is_on_floor():
+			animated_sprite_2d.play("walk")
+	
+	else:
+		animated_sprite_2d.play("Jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 	move_and_slide()
